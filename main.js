70540d36c611bef58e423f8f1c80ba93ca259c64
Patricia Robles <patriciarobles.tech@gmail.com>
2023-01-18 14:18:56 +0000

    add search for all domains

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 import fs from "fs";
+
 const file = fs.readFileSync('test.txt', 'utf8')
+console.log(typeof file)
 //to.string and 'utf8' in the function produce the same result
 
-//search potential emails
+//search potential emails - returns 2010
 const emailRegex = /(@)/g
 const potentialEmails = file.match(emailRegex)
 console.log(potentialEmails.length)
 
 //naive approach
-
 let counter = 0
 for (let i = 0; i < file.length; i++) {
-    if (file.substring(i, (i + 13)) == '@softwire.com ') {
+    if (file.substring(i, (i + 13)) === '@softwire.com ') {
         counter++
     }
 }
 console.log(`Naive approach count: ${counter}`)
-console.log(typeof file)
-
 
 
 //search softwire.com emails - returns 261
 const softwireEmailRegex = /(@softwire\.com)\s/g
 const softwireEmails = file.match(softwireEmailRegex)
-console.log(`Regex approach count: ${softwireEmails.length}`)
+console.log(`Regex approach count Softwire emails: ${softwireEmails.length}`)
 
-//search for all possible domains
-const allDomainRegex = /(\D+)\.(\D) /g
+//search for all possible domains - returns 1825
+const allDomainRegex = /@(\w+)\.(\w+)(\.\w+)?\b/g
+const allDomainEmails = file.match(allDomainRegex)
+console.log(`Regex approach count all emails: ${allDomainEmails.length}`)
+console.log(allDomainEmails)
 
